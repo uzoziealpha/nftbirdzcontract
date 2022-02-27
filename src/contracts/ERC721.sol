fa609f423dad6f615774b086855d1891e89ec00f
Obinna Uzozie <Uzozie.obi@icloud.com>
2022-02-27 13:06:08 +0800

    enable balanceOf to find token owner

--- src/contracts/ERC721.sol
+++ src/contracts/ERC721.sol
@@ -41,9 +41,39 @@ contract ERC721 {
   // create mapping  token ID to the owner 
   //unit - is the calculation of bit so 
     mapping(uint256 => address) private _tokenOwner;
-
-    //create mapping from owner to number of owned tokens 
+   //create mapping from owner to number of owned tokens 
     mapping(address => uint256) private _OwnedTokensCount;
+    
+
+
+
+ ///   @notice Count all NFTs assigned to an Owner
+ ///   @dev
+ ///   @param _owner The address of who we query the balance
+ ///   @return
+  
+   function balanceOf(address _owner) public view returns(uint256) {
+     require(_owner != address(0), 'Token does not exist');
+     return _OwnedTokensCount[_owner];
+   } 
+
+
+ ///   @notice Finding the Owner of an NFT
+ ///   @dev
+ ///   @param _tokenId the id for an NFT
+ ///   @return
+
+     function ownerOf(uint256 _tokenId) external view returns(address) {
+       //this address stores the owner mapping it out with the ID
+       address owner = _tokenOwner[_tokenId];
+       require(owner != address(0), 'Owner does not exist');
+       return owner;
+}
+
+
+   
+
+
 
 
   //Write a function called _mint that takes two arguments
--- src/contracts/KryptoBirdz.sol
+++ src/contracts/KryptoBirdz.sol
@@ -1,46 +1,35 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.4.22 <0.9.0;
 
-import './ERC721Connector.sol';
+import "./ERC721Connector.sol";
 
+//this will store the information that wull run and deploy into the blockchain to test code
+contract KryptoBird is ERC721Connector {
+    //create an array to store the kryptobird nfts
+    string[] public kryptoBirdz;
 
-//this will store the information that wull run and deploy into the blockchain to test code 
-contract Kryptobird is ERC721Connector {
+    mapping(string => bool) _kryptoBirdzExists;
 
-   //create an array to store the kryptobird nfts
-   string[] public kryptoBirdz;
+    function mint(string memory _kryptoBird) public {
+        require(
+            !_kryptoBirdzExists[_kryptoBird],
+            "Error - kryptoBird already exists"
+        );
 
-   mapping(string => bool) _kryptoBirdzExists;
-   
-   function mint(string memory _kryptoBird) public {
+        // deprecated .push  uint _id = KryptoBirdz.push(_kryptoBird);
+        //.push method in solidity no longer returns length
+        kryptoBirdz.push(_kryptoBird);
+        uint256 _id = kryptoBirdz.length - 1;
 
-    require(!_kryptoBirdzExists[_kryptoBird], 
-    'Error - kryptoBird already exists');
+//the variable of the current caller
+        _mint(msg.sender, _id);
 
+        //this will make us access the same mapping
+        _kryptoBirdzExists[_kryptoBird] = true;
+    }
 
-    // deprecated .push  uint _id = KryptoBirdz.push(_kryptoBird);
-    //.push method in solidity no longer returns length
-    kryptoBirdz.push(_kryptoBird);
-    uint _id = kryptoBirdz.length -1;
-
-
-      _mint(msg.sender, _id);
-
-//this will make us access the same mapping
-      _kryptoBirdzExists[_kryptoBird] = true;
-
-
-   }
-
-
-
-
-
-
-   //we must have a name a symbol
-   // initialize this contact to inherit name ans symbol from
-   //erc721metadata. 
-   constructor() ERC721Connector('KryptoBird', 'KBIRDZ')
-   {}
-
+    //we must have a name a symbol
+    // initialize this contact to inherit name ans symbol from
+    //erc721metadata.
+    constructor() ERC721Connector("KryptoBird", "KBIRDZ") {}
 }
