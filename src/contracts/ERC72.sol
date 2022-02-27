0361e5254c433b6db43c6d33c8b1b954fd53f3b6
Obinna Uzozie <Uzozie.obi@icloud.com>
2022-02-27 11:41:52 +0800

    creating the mingting function

--- src/contracts/ERC72.sol
+++ src/contracts/ERC72.sol
@@ -13,6 +13,15 @@ pragma solidity >=0.4.22 <0.9.0;
 
  */
 
+ /**
+     TO BUILD WE 
+     1. Write a function called _mint that takes two arguments
+       (address , tokenId)
+    2. Add internal visibility to the signature
+    3. Set the _tokenOwner of the tokenId to the address to 'to'
+    4. Increase the owner token count by 1 each time the function is called.
+  */
+
 contract ERC721 {
     //,mapping in solididty creates a hash table of pair values 
     //to keep track and setting the token 
@@ -25,6 +34,30 @@ contract ERC721 {
     //create mapping from owner to number of owned tokens 
     mapping(address => uint256) private _OwnedTokensCount;
 
+
+  //Write a function called _mint that takes two arguments
+  
+       //function to check if the token ID already exists
+     function _exists(uint256 tokenId) internal view returns(bool) {
+            //setting address of nft owner to check the address of the tokenOwner at the tokenId
+           address owner = _tokenOwner[tokenId];
+           // return truthiness if the address is not 0
+           return owner != address(0);
+       }
+
+//this function makes the owner get a specific id 
+
+      function _mint(address to, uint256 tokenId) internal {
+        //making the address to a specifc address not invalid
+        require(to != address(0),'ERC721: minting to the zero address');
+        //this will make sure that if it already exist return message
+        require(!_exists(tokenId), 'ERC721: token already minted');
+        // we are adding new address with a token ID with the mint using [tokenId]
+        _tokenOwner[tokenId] = to;
+   //     _OwnedTokensCount [to] = _OwnedTokensCount[to] + 1; 
+    //this helps to keep track of the minting and adding 1 to the count. 
+        _OwnedTokensCount[to] += 1;
+    }
 }
 
 
